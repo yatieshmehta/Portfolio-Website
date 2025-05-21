dff0e4f0994f6b19194ce8a9f1a05c8f2eda3ca8
Yatiesh Mehta <yatiesh.mehta@gmail.com>
2025-05-21 12:14:06 -0400

    Fixed google icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Yatiesh Mehta',
   description: 'Portfolio website of Yatiesh Mehta',
-  icons: {
-    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><circle cx="50" cy="50" r="45" fill="%233b82f6"/><text x="50" y="55" font-size="65" font-family="Georgia" fill="white" text-anchor="middle" dominant-baseline="middle">λ</text></svg>',
-  },
+  icons: [
+    { rel: 'icon', type: 'image/svg+xml', url: '/icon.svg' },
+    { rel: 'icon', url: '/icon.ico', sizes: 'any' },
+    ],
 }
 
 export default function RootLayout({
